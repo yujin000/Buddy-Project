e5f6929e36542adbcd4bf43173b5fe21609fe6d4
yechan <yechan3110@gmail.com>
2022-12-14 02:25:02 +0900

    Feat : index 이동 시 세션 확인해서 index.html 또는 indexError.html로 이동하도록 설정

--- src/main/java/com/fivet/buddy/controller/HomeController.java
+++ src/main/java/com/fivet/buddy/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.fivet.buddy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private HttpSession session;
     @RequestMapping("/")
-    public String home() {
-        return "index";
+    public String home(Model model) {
+        // 세션이 있으면 mypage.html, 세션 없으면 index로 이동(이중 로그인 방지)
+        if(session.getAttribute("memberSeq") == null){
+            return "index";
+        }else{
+            model.addAttribute("memberSeq",session.getAttribute("memberSeq"));
+            return "indexError";
+        }
+
     }
 }

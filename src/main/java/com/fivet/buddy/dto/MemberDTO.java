f2f680256463b68914eb510457b42002e8a0538a
yechan <yechan3110@gmail.com>
2022-12-13 09:16:24 +0900

    Feat : MemberDTO 생성 , getter/setter/constructor

--- src/main/java/com/fivet/buddy/dto/MemberDTO.java
+++ src/main/java/com/fivet/buddy/dto/MemberDTO.java
@@ -1,4 +1,20 @@
 package com.fivet.buddy.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
+    private int memberSeq;
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private String memberPhone;
+    private Timestamp memberSignupDate;
 }

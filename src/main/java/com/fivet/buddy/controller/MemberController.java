ecce8067f663ae6a239f8f1c6b6c14a38224498a
yechan <yechan3110@gmail.com>
2022-12-13 10:36:47 +0900

    Feat : 회원가입 페이지 이동 추가

--- src/main/java/com/fivet/buddy/controller/MemberController.java
+++ src/main/java/com/fivet/buddy/controller/MemberController.java
@@ -2,16 +2,22 @@ package com.fivet.buddy.controller;
 
 import com.fivet.buddy.dto.MemberDTO;
 import com.fivet.buddy.services.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("/member/")
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    private Logger logger = LoggerFactory.getLogger(MemberController.class);
 
     // ExceptionHandler
     @ExceptionHandler(Exception.class)
@@ -29,6 +35,7 @@ public class MemberController {
     // 회원가입 (signUp)
     @RequestMapping("signUp")
     public String signUp(MemberDTO memberDto) throws Exception{
+        logger.info(memberDto.getMemberId() + " : " + memberDto.getMemberPw() + " : " + memberDto.getMemberName() + " : " + memberDto.getMemberPhone());
         memberService.signUp(memberDto);
         return "redirect:/";
     }

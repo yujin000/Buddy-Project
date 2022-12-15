fc8e796cf07e6723c85e0351c5c1fe2664b0bf25
yechan <yechan3110@gmail.com>
2022-12-15 16:52:31 +0900

    Feat : 이메일 인증 난수 변경

--- src/main/java/com/fivet/buddy/controller/EmailController.java
+++ src/main/java/com/fivet/buddy/controller/EmailController.java
@@ -3,11 +3,10 @@ package com.fivet.buddy.controller;
 import com.fivet.buddy.util.EmailUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Map;
+import java.util.Random;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,11 +18,26 @@ public class EmailController {
 
     @PostMapping("sendEmail")
     public Map<String, Object> sendEmail(@RequestBody Map<String, Object> params){
-        log.info("email params={}", params);
-
         return emailUtil.sendEmail( (String) params.get("userId")
                 , (String) params.get("subject")
                 , (String) params.get("body")
         );
+    }
+    @ResponseBody
+    @PostMapping("emailProve")
+    public String emailProve() throws  Exception{
+        String ranVal = createEmailKey();
+        System.out.println(ranVal);
+        return ranVal;
+    }
+
+    public String createEmailKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 6; i++) { // 이메일 인증코드 6자리
+            key.append((rnd.nextInt(10)));
+        }
+        return key.toString();
     }
 }

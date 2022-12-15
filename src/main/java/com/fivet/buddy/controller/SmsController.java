91313a55a01c366e7bd22e8a9516b75a4b3776f7
yechan <yechan3110@gmail.com>
2022-12-15 16:53:31 +0900

    Feat : 숫자 + 영문 조합 난수 20자리 생성 메서드

--- src/main/java/com/fivet/buddy/controller/SmsController.java
+++ src/main/java/com/fivet/buddy/controller/SmsController.java
@@ -5,6 +5,7 @@ import com.fivet.buddy.dto.SmsResponseDTO;
 import com.fivet.buddy.services.SmsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ public class SmsController {
 
     private final SmsService smsService;
 
+
     @GetMapping("/send")
     public String getSmsPage() {
         return "sendSms";
@@ -33,12 +35,10 @@ public class SmsController {
     @ResponseBody
     @PostMapping("/sms/send")
     public String sendSms(@RequestBody SmsDTO smsDto, Model model) throws JsonProcessingException, RestClientException, URISyntaxException, InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
-        System.out.println(smsDto.getTo());
         // 랜덤 번호 변수
         String confirmVal = createSmsKey();
         // set 랜덤 번호
         smsDto.setContent("[Buddy] 회원가입 인증번호 [" + confirmVal + "]를 화면에 입력해주세요.");
-        System.out.println(confirmVal);
         // 문자 발송
         SmsResponseDTO response = smsService.sendSms(smsDto);
         // 응답확인(요청보낸 아이디, 요청 시간, 상태)
@@ -49,13 +49,29 @@ public class SmsController {
     }
 
     // 랜덤 번호 5자리 생성
-    public static String createSmsKey() {
+    public String createSmsKey() {
         StringBuffer key = new StringBuffer();
         Random rnd = new Random();
 
         for (int i = 0; i < 5; i++) { // 인증코드 5자리
             key.append((rnd.nextInt(10)));
         }
+        return key.toString();
+    }
+
+    // 숫자+영문 20자리 생성(초대에 써야징)
+    public String smsTimeOutVal() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            if (rnd.nextBoolean()) {
+                key.append((char) ((int) (rnd.nextInt(26)) + 97));
+            } else {
+                key.append((rnd.nextInt(10)));
+            }
+        }
+
         return key.toString();
     }
 }

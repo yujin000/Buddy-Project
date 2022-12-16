a55d578438aa3f77f931c73525717b6cbabb82e6
yechan <yechan3110@gmail.com>
2022-12-16 12:02:11 +0900

    Feat : 회원 초대 이메일 코드 생성

--- src/main/java/com/fivet/buddy/util/EmailUtil.java
+++ src/main/java/com/fivet/buddy/util/EmailUtil.java
@@ -4,4 +4,6 @@ import java.util.Map;
 
 public interface EmailUtil {
     Map<String, Object> sendEmail(String toAddress, String subject, String body);
+
+    Map<String, Object> sendInviteEmail(String toAddress, String subject, String body);
 }

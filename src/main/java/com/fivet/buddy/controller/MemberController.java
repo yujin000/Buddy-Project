eab99505f96010d0b55e5c84144959e81373b98f
syj <sinyj827@naver.com>
2022-12-21 15:40:54 +0900

    Save : 풀을 위한 커밋 (MemberController.java) 주석 추가, (myprofile.html) 프로필 이미지 파일 확장자 제한, 용량 제한, Fix : (index.html)계정설정 버튼 추가, (myprofile.css) 필요없는 css 삭제

--- src/main/java/com/fivet/buddy/controller/MemberController.java
+++ src/main/java/com/fivet/buddy/controller/MemberController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;

35e4665ce71821240edf52378574b0f08acca5e4
yechan <yechan3110@gmail.com>
2022-12-21 15:21:40 +0900

    Feat : drive에서 파일 업로드 기능 추가

---
+++ src/main/java/com/fivet/buddy/controller/PersonalFileController.java
@@ -0,0 +1,46 @@
+package com.fivet.buddy.controller;
+
+import com.fivet.buddy.services.PersonalFolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/personalFile/")
+public class PersonalFileController {
+
+    @Autowired
+    private PersonalFolderService personalFolderService;
+
+    // 나중에 AWS 열면 경로 변경할 것(application.properties 에서)
+    @Value("${spring.servlet.multipart.location}")
+    private String uploadFilePath;
+
+
+    @RequestMapping("uploadFromBtn")
+    public String uploadFromBtn(MultipartFile multipartFile) throws Exception{
+
+        File filePath = new File(uploadFilePath);
+
+        if(!filePath.exists()) {
+            filePath.mkdir();
+        }
+
+        String oriName = multipartFile.getOriginalFilename();
+        String sysName = UUID.randomUUID() + "_" + oriName;
+
+        multipartFile.transferTo(new File(filePath+"/"+sysName));
+
+        // personal_file 테이블에 insert
+        //personalFolderService.uploadFromBtn(oriName,sysName);
+
+        return "redirect:/drive/toFileDrive";
+    }
+}

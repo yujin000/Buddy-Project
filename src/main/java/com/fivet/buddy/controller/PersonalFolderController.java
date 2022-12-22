83a2b37d174df82ece398390a081671af3ffa6d1
yechan <yechan3110@gmail.com>
2022-12-22 14:13:33 +0900

    Feat : 회원가입 시 폴더 생성, 하위 폴더 생성 기능 추가

--- src/main/java/com/fivet/buddy/controller/PersonalFolderController.java
+++ src/main/java/com/fivet/buddy/controller/PersonalFolderController.java
@@ -1,19 +1,14 @@
 package com.fivet.buddy.controller;
 
-import com.fivet.buddy.dto.PersonalFolderDTO;
 import com.fivet.buddy.services.PersonalFolderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/personalFolder/")
@@ -25,10 +20,6 @@ public class PersonalFolderController {
     @Autowired
     private PersonalFolderService personalFolderService;
 
-    // 나중에 AWS 열면 경로 변경할 것(application.properties 에서)
-    @Value("${spring.servlet.multipart.location}")
-    private String uploadFilePath;
-
     // ExceptionHandler
     @ExceptionHandler
     public String exceptionHandler(Exception e) {
@@ -39,27 +30,22 @@ public class PersonalFolderController {
     // 폴더 생성
     @ResponseBody
     @RequestMapping("addFolder")
-    public boolean addFolder(String folderName) throws Exception {
+    public boolean addFolder(String folderName,String parentKey) throws Exception {
         // 폴더 중복 체크( true : 존재함, false : 존재하지 않음)
         boolean folderExists = personalFolderService.isFolderExists(folderName);
 
         // 폴더가 존재하지 않는 경우( 사용이 가능한 경우 )
         if (!folderExists) {
-            PersonalFolderDTO personalFolderDto = new PersonalFolderDTO();
-
-            // 세션에 있는 memberSeq값 폴더 dto에 입력
-            personalFolderDto.setPersonalFolderMemberSeq(Integer.parseInt(session.getAttribute("memberSeq").toString()));
+            // 생성할 폴더 경로
+            String uploadFilePath = personalFolderService.searchPath(parentKey);
             // 폴더 생성
             File file = new File(uploadFilePath + folderName);
             file.mkdir();
-
-            personalFolderDto.setPersonalFolderName(folderName);
-
             // personal_folder 테이블에 insert
-            personalFolderService.insertNewFolder(personalFolderDto);
+            personalFolderService.insertNewFolder(folderName,parentKey,uploadFilePath);
             return true;
         } else {
-            // 폴더가 이미 존재하는 경우( 사용이 불가능한 경우)
+            // 폴더가 이미 존재하는 경우( 사용이 불가능한 경우 )
             return false;
         }
     }

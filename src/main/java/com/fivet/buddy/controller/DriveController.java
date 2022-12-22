c8b643ac323c102b223f82e6b8d9a37f7c9c8ac8
yechan <yechan3110@gmail.com>
2022-12-22 14:11:22 +0900

    Feat : 하위폴더 이동, model 기능 추가

--- src/main/java/com/fivet/buddy/controller/DriveController.java
+++ src/main/java/com/fivet/buddy/controller/DriveController.java
@@ -1,6 +1,8 @@
 package com.fivet.buddy.controller;
 
+import com.fivet.buddy.dto.PersonalFileDTO;
 import com.fivet.buddy.dto.PersonalFolderDTO;
+import com.fivet.buddy.services.PersonalFileService;
 import com.fivet.buddy.services.PersonalFolderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,9 @@ public class DriveController {
     private PersonalFolderService personalFolderService;
 
     @Autowired
+    private PersonalFileService personalFileService;
+
+    @Autowired
     private HttpSession session;
 
     // Exception Handler
@@ -31,14 +36,22 @@ public class DriveController {
     // fileDrive.html로 이동
     @RequestMapping("toFileDrive")
     public String toFileDrive(Model model) throws Exception{
-        List<PersonalFolderDTO> myFolders = personalFolderService.selectMyFolders(session.getAttribute("memberSeq").toString());
+        int memberSeq = Integer.parseInt(session.getAttribute("memberSeq").toString());
+
+        List<PersonalFolderDTO> myFolders = personalFolderService.selectMyFolders(memberSeq);
+        List<PersonalFileDTO> myFiles = personalFileService.selectMyFiles(memberSeq);
+        model.addAttribute("parentKey",personalFolderService.myBasicFolder(memberSeq));
         model.addAttribute("myFolders",myFolders);
+        model.addAttribute("myFiles",myFiles);
         return "drive/fileDrive";
     }
 
     // 폴더 세부내역으로 이동
     @RequestMapping("detailDrive")
-    public String detailDrive() throws Exception{
+    public String detailDrive(String resourceKey, Model model) throws Exception{
+        List<PersonalFolderDTO> myFolders = personalFolderService.selectChildFolders(resourceKey);
+        model.addAttribute("parentKey",resourceKey);
+        model.addAttribute("myFolders",myFolders);
         return "drive/detailDrive";
     }
 }

e45ae3fa04fd537209e1a6750187f3f2b6bedae6
yechan <yechan3110@gmail.com>
2022-12-22 14:09:44 +0900

    Feat : basic 테이블에서 key값 출력하기

--- src/main/java/com/fivet/buddy/dao/BasicFolderDAO.java
+++ src/main/java/com/fivet/buddy/dao/BasicFolderDAO.java
@@ -1,8 +1,11 @@
 package com.fivet.buddy.dao;
 
+import com.fivet.buddy.dto.MemberDTO;
 import com.fivet.buddy.mapper.BasicFolderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class BasicFolderDAO {
@@ -11,7 +14,12 @@ public class BasicFolderDAO {
     private BasicFolderMapper basicFolderMapper;
 
     // 신규 회원가입 시 기본 폴더 생성
-    public void newBasicFolder(int basicFolderMemberSeq) throws Exception{
-        basicFolderMapper.newBasicFolder(basicFolderMemberSeq);
+    public void newBasicFolder(Map<String, Object> map) throws Exception{
+        basicFolderMapper.newBasicFolder(map);
     };
+
+    // 기본 폴더에 있는 폴더 key값 가져오기
+    public String myBasicFolder(int memberSeq) throws Exception{
+        return basicFolderMapper.myBasicFolder(memberSeq);
+    }
 }

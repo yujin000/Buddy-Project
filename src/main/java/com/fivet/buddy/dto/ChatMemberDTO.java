392559143bf75632e9fe9cfcf998ae8fa78e003d
박세진 <crazywarden@naver.com>
2022-12-26 12:03:52 +0900

    (ver 4.7.1) Fix : 회원 한명이 여러 팀에 속한 경우, 채팅방 입장이 되지 않았던 버그 수정.

--- src/main/java/com/fivet/buddy/dto/ChatMemberDTO.java
+++ src/main/java/com/fivet/buddy/dto/ChatMemberDTO.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 public class ChatMemberDTO {
     private int chatRoomSeq;
     private int memberSeq;
+    private int teamSeq;
     private Timestamp chatJoinDate;
     private Timestamp recentViewDate;
 }

ef4071b4af57cbf36c59c62d25b0967fb280beef
csh <ayocjs@naver.com>
2022-12-26 20:12:51 +0900

    Feat : 공지사항 및 뉴스 기능 추가

---
+++ src/main/java/com/fivet/buddy/dao/NoticeDAO.java
@@ -0,0 +1,22 @@
+package com.fivet.buddy.dao;
+
+import com.fivet.buddy.dto.NoticeDTO;
+import com.fivet.buddy.mapper.NoticeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class NoticeDAO {
+
+    @Autowired
+    private NoticeMapper noticemapper;
+
+    public List<NoticeDTO> select() throws Exception{
+        return noticemapper.select();
+    }
+    public NoticeDTO selectDetail(NoticeDTO noticeDto) throws Exception{
+        return noticemapper.selectDetail(noticeDto);
+    }
+}

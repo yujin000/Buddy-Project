162eb092ba61c93d5db615453690480d9496704e
yechan <yechan3110@gmail.com>
2022-12-30 08:55:53 +0900

    New : BasicFolderDTO 생성

---
+++ src/main/java/com/fivet/buddy/dto/BasicFolderDTO.java
@@ -0,0 +1,17 @@
+package com.fivet.buddy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicFolderDTO {
+    private String basicFolderKey;
+    private int basicFolderMemberSeq;
+    private String basicFolderName;
+    private int basicFolderByte;
+}

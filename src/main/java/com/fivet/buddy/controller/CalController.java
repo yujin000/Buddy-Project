ec21721df3e0a37f47560be6f0243666ff6a8f39
Jung-taekku-dev <xorrn93@gmail.com>
2023-01-03 10:51:35 +0900

    Refactor

--- src/main/java/com/fivet/buddy/controller/CalController.java
+++ src/main/java/com/fivet/buddy/controller/CalController.java
@@ -4,8 +4,6 @@ import com.fivet.buddy.dto.CalDTO;
 import com.fivet.buddy.services.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

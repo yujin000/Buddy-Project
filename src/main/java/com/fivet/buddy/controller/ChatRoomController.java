c675c95477072b73cc475bd5df5cc9a8ff7ec86e
csh <ayocjs@naver.com>
2023-01-03 17:46:00 +0900

    Feat : 팀원 닉네임 통합작업

--- src/main/java/com/fivet/buddy/controller/ChatRoomController.java
+++ src/main/java/com/fivet/buddy/controller/ChatRoomController.java
@@ -44,14 +44,30 @@ public class ChatRoomController {
     @Autowired
     private ChatMemberService chatMemberService;
 
+    @Autowired
+    private MemberService memberService;
+
     //채팅방 입장
     @RequestMapping("join")
-    public String insertChatMsg(int chatRoomSeq, Model model) {
+    public String insertChatMsg(TeamMemberDTO teamMemberDto,int chatRoomSeq, Model model) throws Exception {
+        teamMemberDto.setTeamSeq((int)session.getAttribute("teamSeq"));
+        teamMemberDto.setMemberSeq((int)session.getAttribute("memberSeq"));
+        // 회원 번호를 이용하여 팀 DTO값을 불러옴.
+        teamMemberDto = teamMemberService.selectOne(teamMemberDto);
+        model.addAttribute("teamMemberInfo", teamMemberDto);
+
         model.addAttribute("chatRoomSeq",chatRoomSeq);
         model.addAttribute("chatMsgList", chatMsgService.selectChatMsg(chatRoomSeq));
         model.addAttribute("chatMemberList",teamMemberService.selectChatMember(chatRoomSeq));
-        String memberImgSysName = chatMemberService.selectChatMemberImg((int) session.getAttribute("memberSeq"),chatRoomSeq);
-        model.addAttribute("memberImgSysName",memberImgSysName);
+
+        //프로필 이미지 출력
+        String memberImgSysName = memberService.selectProfileImg(String.valueOf(session.getAttribute("memberSeq")));
+        if(memberImgSysName.equals("/static/img/defaultProfileImg.png")){
+            model.addAttribute("memberImgSysName",memberImgSysName);
+        }else{
+            memberImgSysName = "/member/selectProfileImg/"+memberImgSysName;
+            model.addAttribute("memberImgSysName",memberImgSysName);
+        }
         return ("/team/teamChating");
     }
 
